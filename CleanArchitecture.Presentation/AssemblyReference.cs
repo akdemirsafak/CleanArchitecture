7ed5b94ccae29ebdb611abcf55c1b5d1f4d7bad7
akdemirsafak <akdemirsafak@gmail.com>
2024-02-29 10:16:14 +0300

    Presentation katmanında controller oluşturulması ve api'a bildirilmesi.

---
+++ CleanArchitecture.Presentation/AssemblyReference.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+namespace CleanArchitecture.Presentation;
+
+public static class AssemblyReference //Presentation katmanının dll haline erişim sağlama
+{
+    public static readonly Assembly assembly = typeof(Assembly).Assembly;
+}
---
+++ CleanArchitecture.Presentation/Controllers/ValuesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchitecture.Presentation.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public sealed class ValuesController : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        return Ok("Presentation'a api işlemi");
+    }
+}
