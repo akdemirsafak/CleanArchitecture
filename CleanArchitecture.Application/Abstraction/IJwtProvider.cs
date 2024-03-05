4dba7cd6a331e0fa6280b37ddd0c10403930068d
akdemirsafak <akdemirsafak@gmail.com>
2024-03-06 01:45:03 +0300

    Create Token added.

---
+++ CleanArchitecture.Application/Abstraction/IJwtProvider.cs
@@ -0,0 +1,8 @@
+﻿using CleanArchitecture.Domain.Entities;
+
+namespace CleanArchitecture.Application.Abstraction;
+
+public interface IJwtProvider
+{
+    string CreateToken(AppUser user); //Token ile birlikte rollerin taşınması yanlıştır.Kullanıcı bilgileriyle db'den sorgulayıp kullanmak daha güvenlidir.
+}
---
+++ CleanArchitecture.Infrastructure/Authentication/JwtProvider.cs
@@ -0,0 +1,43 @@
+﻿using CleanArchitecture.Application.Abstraction;
+using CleanArchitecture.Domain.Entities;
+using CleanArchitecture.Infrastructure.Options.Auth;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace CleanArchitecture.Infrastructure.Authentication;
+
+public sealed class JwtProvider : IJwtProvider
+{
+    private readonly JwtOptions _jwtOptions;
+
+    public JwtProvider(IOptions<JwtOptions> jwtOptions)
+    {
+        _jwtOptions = jwtOptions.Value;
+    }
+
+    public string CreateTokenAsync(AppUser user)
+    {
+        SymmetricSecurityKey securityKey = new (Encoding.UTF8.GetBytes(_jwtOptions.SecurityKey));
+
+        var claims=new Claim[]{
+            new Claim(ClaimTypes.Email,user.Email),
+            new(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+        };
+
+        JwtSecurityToken jwtSecurityToken= new (
+            _jwtOptions.Issuer,
+            //_jwtOptions.Audiences[0],
+            claims:claims,
+            notBefore:DateTime.Now,
+            expires:DateTime.Now.AddHours(1),
+            signingCredentials:new SigningCredentials(securityKey,SecurityAlgorithms.HmacSha256Signature)
+            );
+        string token= new JwtSecurityTokenHandler().WriteToken(jwtSecurityToken);
+        return token;
+    }
+}
