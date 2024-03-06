f2ec68e000edbad729fa76582f381af0e6db3480
akdemirsafak <akdemirsafak@gmail.com>
2024-03-06 22:49:21 +0300

    GetCarsQueryFix

--- CleanArchitecture.Application/Features/CarFeatures/Queries/GetAll/GetCarsQuery.cs
+++ CleanArchitecture.Application/Features/CarFeatures/Queries/GetAll/GetCarsQuery.cs
@@ -3,4 +3,4 @@ using MediatR;
 
 namespace CleanArchitecture.Application.Features.CarFeatures.Queries.GetAll;
 
-public sealed record GetCarsQuery():IRequest<List<Car>>;
+public sealed record GetCarsQuery():IRequest<IList<Car>>;
